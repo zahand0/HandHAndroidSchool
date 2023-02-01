f04099f2a97e09ebd117eba7b62f033b2ac1a225
Sergey <mulichev@gmail.com>
2023-02-01 21:48:21 +0300

    created activities

---
+++ Lesson_1/Task1/app/src/main/java/com/zahand0/task1/GreenActivity.kt
@@ -0,0 +1,11 @@
+package com.zahand0.task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_green)
+    }
+}
---
+++ Lesson_1/Task1/app/src/main/java/com/zahand0/task1/RedActivity.kt
@@ -0,0 +1,11 @@
+package com.zahand0.task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_red)
+    }
+}
