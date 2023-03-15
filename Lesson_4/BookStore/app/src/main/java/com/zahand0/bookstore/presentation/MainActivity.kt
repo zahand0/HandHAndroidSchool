fb4816dfb51df7891f74e89f73f44263e9eac718
Sergey <mulichev@gmail.com>
2023-03-15 23:20:55 +0300

    reformatting

--- Lesson_4/BookStore/app/src/main/java/com/zahand0/bookstore/presentation/MainActivity.kt
+++ Lesson_4/BookStore/app/src/main/java/com/zahand0/bookstore/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.zahand0.bookstore.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.zahand0.bookstore.R
