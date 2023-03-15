eb1c87ed313ca2f64068255ca6c46efdc12a66fd
Sergey <mulichev@gmail.com>
2023-03-15 19:56:52 +0300

    added main activity

---
+++ Lesson_4/BookStore/app/src/main/java/com/zahand0/bookstore/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zahand0.bookstore.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.zahand0.bookstore.R
+import com.zahand0.bookstore.presentation.screen.library.LibraryFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.commit {
+            add<LibraryFragment>(R.id.container)
+        }
+    }
+}
